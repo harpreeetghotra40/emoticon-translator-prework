76a785c91ddacd3b2c688d08a7e32f0ed54442f3
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-23 22:56:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,14 @@
 # require modules here
 require "yaml"
 
-def load_library(path)
-  emojis = YAML.load_file(path)
-  emojis
+def load_library(file_path)
+  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  result
 end
 
 def get_japanese_emoticon
